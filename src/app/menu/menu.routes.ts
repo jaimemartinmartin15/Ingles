1d5813c392dd94f91cebce56f838769d5b9239df
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2024-06-14 18:44:12 +0200

    Fix meta tag description, added npm script.

--- src/app/menu/menu.routes.ts
+++ src/app/menu/menu.routes.ts
@@ -6,6 +6,12 @@ export const MENU_ROUTES: Routes = [
     path: '',
     component: MenuComponent,
     title: 'Practica inglés',
+    data: {
+      metaTags: {
+        description: 'Aprende las formas verbales, el alfabeto y su pronunciación y repasa.',
+        keywords: ['verbos', 'ingles', 'alfabeto', 'pronunciacion', 'repaso'],
+      }
+    }
   },
   {
     path: '**',
